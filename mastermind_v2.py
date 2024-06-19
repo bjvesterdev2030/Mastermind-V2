145484241c3c8318ff8538fba378d415a8196af5
Braden Vester <brvester@iupui.edu>
2024-06-19 19:07:48 -0400

    Created a crappy looking menu.

--- mastermind_v2.py
+++ mastermind_v2.py
@@ -1,4 +1,9 @@
 from pycolor import style_text
+import datetime as dt
+from time import sleep
+from os import system
+
+
 # Special Characters : ╚ ╗ ╔ ╝║ ═ ╣ ╠ ╩ ╦ ●
 
 
@@ -15,8 +20,60 @@ class game_menu():
         with open("logo.txt", 'rt') as file:
             logo = file.read()
             return logo
-    
-    
+        
+    def display_menu_screen(self):
+        system("cls")
+        print(self.logo)
+        
+        current_time = dt.datetime.now()
+        print(" " + current_time.strftime("%A, %B %d %I:%M %p"))
+        print(f" Active Player : {self.active_profile}")
+        
+        line_break = " ________________________________\n "
+        print(line_break)
+        
+        menu_options = [
+            {"option_long":"Set Active Profile",
+             "option_short":"A"},
+            {"option_long":"Start Game",
+             "option_short":"G"},
+            {"option_long":"Show High Scores",
+             "option_short":"H"},
+            {"option_long":"Create New Profile",
+             "option_short":"C"},
+            {"option_long":"Delete Profile",
+             "option_short":"D"},
+            {"option_long":"Exit Game",
+             "option_short":"X"},
+        ]
+        
+        for option in menu_options:
+            print(f" [{option['option_short']}] {option['option_long']}")
+            
+        print(line_break)
+        
+        # TODO: Make sure this ish is valid
+        menu_selection = str(input(" Selection : "))
+        print(line_break)
+        
+        self.menu_selection_handler(menu_selection)
+        
+    def menu_selection_handler(self, menu_selection:str):
+        match menu_selection:
+            case "A":
+                print("Setting Active Profile")
+            case "G":
+                print("Starting Game")
+            case "H":
+                print("Showing High Scores")
+            case "C":
+                print("Creating New Profile")
+            case "D":
+                print("Deleting Profile")
+            case "X":
+                print("Exiting Game")
+                sleep(.25)
+                exit()
     
 class player_guess():
     def __init__(self):
@@ -32,7 +89,7 @@ class game_board():
     
 def main():
     menu = game_menu()
-    print(menu.logo)
+    menu.display_menu_screen()
 
 if __name__ == "__main__":
     main()
