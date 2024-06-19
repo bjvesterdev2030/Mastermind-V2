6bc517f7a30b874669f12168fb6982de3cf99083
Braden Vester <brvester@iupui.edu>
2024-06-19 12:12:20 -0400

    Added basic class structure, logo.

--- mastermind_v2.py
+++ mastermind_v2.py
@@ -1,11 +1,38 @@
 from pycolor import style_text
+# Special Characters : ╚ ╗ ╔ ╝║ ═ ╣ ╠ ╩ ╦ ●
 
-class game():
+
+class player_profile():
+    def __init__(self):
+        pass
+
+class game_menu():
+    def __init__(self):
+        self.logo = self.recall_logo()
+        self.active_profile = None
+        
+    def recall_logo(self):
+        with open("logo.txt", 'rt') as file:
+            logo = file.read()
+            return logo
+    
+    
+    
+class player_guess():
+    def __init__(self):
+        pass
+
+class code():
+    def __init__(self):
+        pass
+
+class game_board():
     def __init__(self):
         pass
     
 def main():
-    pass
+    menu = game_menu()
+    print(menu.logo)
 
 if __name__ == "__main__":
     main()
